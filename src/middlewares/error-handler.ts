a7eb553d55e997e66b990d7c1f20efb8789476d1
ctq3103 <56945989+ctq3103@users.noreply.github.com>
2020-08-11 11:27:56 +0700

    updates

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -40,8 +40,7 @@ export const errorHandler = (
 	if (err instanceof CustomError) {
 		return res.status(err.statusCode).send({ errors: err.serializeErrors() });
 	}
-
-	res.status(400).send({
+	return res.status(400).send({
 		errors: [{ message: 'Something went wrong!' }],
 	});
 };
