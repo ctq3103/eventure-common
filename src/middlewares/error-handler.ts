782fd297bd9b4f74b88bcd2e85051a89cead03ec
ctq3103 <56945989+ctq3103@users.noreply.github.com>
2020-08-11 10:49:20 +0700

    Updates

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -1,8 +1,10 @@
 import { Response, Request, NextFunction } from 'express';
 import { CustomError } from '../errors/custom-error';
+import { Error as MongooseError } from 'mongoose';
+import { MongoError } from 'mongodb';
 
 export const errorHandler = (
-	err: Error,
+	err: Error | MongooseError,
 	req: Request,
 	res: Response,
 	next: NextFunction
@@ -10,8 +12,38 @@ export const errorHandler = (
 	if (err instanceof CustomError) {
 		return res.status(err.statusCode).send({ errors: err.serializeErrors() });
 	}
+
+	//Mongoose bad ObjectId
+	if (err instanceof MongooseError.CastError) {
+		const message = `Resource not found with ID of ${err.value}`;
+
+		res.status(404).send({
+			errors: [{ message }],
+		});
+	}
+
+	//Mongoose duplicate key
+	if (err instanceof MongoError && err.code === 11000) {
+		const message = 'Duplicate field value entered';
+
+		res.status(400).send({
+			errors: [{ message }],
+		});
+	}
+
+	//Mongoose validation error
+	if (err instanceof MongooseError.ValidationError) {
+		const message = Object.values(err.errors).map((val) => val.message);
+		res.status(400).send({
+			errors: [{ message }],
+		});
+	}
+
 	console.error(err);
+
 	res.status(400).send({
 		errors: [{ message: 'Something went wrong!' }],
 	});
+
+	next();
 };
